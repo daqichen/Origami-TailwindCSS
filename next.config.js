a88434df580a73874576613b11bdc5cf867e4ef8
daqichen <daqichen@live.unc.edu>
2023-12-28 16:32:22 -0500

    add client side render for util function

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: 'https://daqichen.github.io/year2024'
+    basePath: '/year2024'
 }
 
 module.exports = nextConfig

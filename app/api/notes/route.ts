6d0518ee53dc66d2e3fc0300d2c874094524d499
daqichen <daqichen@live.unc.edu>
2024-01-08 23:57:51 -0500

    syntax

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -2,5 +2,5 @@ import getConfig from "next/config";
 const { serverRuntimeConfig } = getConfig();
 
 export async function GET() {
-    return Response.json({NOTES: JSON.parse(serverRuntimeConfig?.NOTES ?? {})});
+    return Response.json({NOTES: JSON.parse(serverRuntimeConfig?.NOTES ?? "{}")});
   }

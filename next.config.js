e3c9125bd4067366188da13fd682e67fb2a55f77
daqichen <daqichen@live.unc.edu>
2024-03-01 21:31:46 -0500

    feature-basepath: reset to /

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    basePath: '/year2024',
     serverRuntimeConfig: {
         ACCESS_USER: process?.env?.ACCESS_USER,
         ACCESS_CODE: process?.env?.ACCESS_CODE,
